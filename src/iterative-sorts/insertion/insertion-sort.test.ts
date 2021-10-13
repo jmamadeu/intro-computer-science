a0d2b0580e2ad631b296ae3cea9f15e96dceafab
jmamadeu <jmamadeu2000@gmail.com>
2021-10-13 13:46:01 +0100

    :zap: chore: rename test title

--- src/iterative-sorts/insertion/insertion-sort.test.ts
+++ src/iterative-sorts/insertion/insertion-sort.test.ts
@@ -1,6 +1,6 @@
 import { insertionSort } from './insertion-sort';
 
-describe('Bubble Sort', () => {
+describe('Insertion Sort', () => {
   test('should be equal', () => {
     expect(insertionSort([10, 6, 5, 4, 3, 2, 7, 8])).toEqual([
       2, 3, 4, 5, 6, 7, 8, 10,
